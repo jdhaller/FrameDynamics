8e425378d2d85395a1ff3eab2bec764335d768e7
Jens <jhaller@gmx.de>
2022-11-08 13:10:52 +0100

    added function to load shapes in xy-amplitude format

--- FrameDynamics/Frame.py
+++ FrameDynamics/Frame.py
@@ -6,7 +6,7 @@ Institution: Karlsruhe Institute of Technology
 
 """
 
-
+import re
 import multiprocessing as mp
 import time
 import numpy as np
@@ -296,17 +296,102 @@ class Frame():
             if end in line:
                 flag = False
             if flag:
-                line = line.split(separator)
+
+                # substitute whitespaces at the beginning
+                line = re.sub("^\s*", "", line)
+
+                # split by separator (flanked by whitespaces)
+                line = re.split("\s*"+separator+"\s*", line)
+
                 try:
                     line = [float(slic) for slic in line]
                 except:
                     line = [slic.replace(' ', '') for slic in line]
+
                 data.append(line)
             if start in line:
                 flag = True
 
         return data
     # ====================================================================
+
+
+    @classmethod
+    def load_shape2(cls, path_to_file: str, separator: str=" ", \
+                comment: str="#") -> list:
+        """
+        Load a pulse shape from file in xy-format.
+
+        Args:
+            path_to_file (str): specifiy path to file (including filename).
+
+            separator (str, optional): character that is used to separate
+                amplitude and phase in file of shaped pulse. Defaults to ",".
+
+            comment (str, optional): specifies comments. Defaults to "#".
+
+        Returns:
+            max_amp (float): maximum amplitude
+            length (float): pulse length
+            shape (list): list of shaped pulse ([ [amp, ph], ..., [amp, ph] ])
+        """
+
+        data = []
+
+        with open(path_to_file, 'r') as tmp:
+            load_data = tmp.read()
+        load_data = load_data.split('\n')
+
+        for line in load_data:
+        
+            # don't use comments, marked by first char 
+            # (not counting whitespaces) 
+            if not re.search(r"\s*"+comment, line):
+                
+                # substitute whitespaces at the beginning
+                line = re.sub("^\s*", "", line)
+
+                # split by separator (flanked by whitespaces)
+                line = re.split("\s*"+separator+"\s*", line)
+
+                try:
+                    line = [float(slic) for slic in line]
+                    data.append(line)
+
+                except ValueError:
+                    if line == ['']: 
+                        pass
+                    else: 
+                        raise TypeError("Unable to convert to float: "+\
+                                        "Check separator!")
+            
+        return cls.convert2Bruker( np.array(data) )
+    # ====================================================================
+
+
+    @staticmethod
+    def convert2Bruker(data):
+        
+        """
+        Converts pulse shapes in xy-amplitude format to Bruker format.
+
+        Returns:
+            float: maximum amplitude
+            float: pulse length
+            list: pulse shape in Bruker format
+
+        """
+
+        amplitudes = np.sqrt(data[:,0]**2 + data[:,1]**2)
+        max_amp = max(amplitudes)
+        amplitudes = amplitudes / max_amp * 100
+
+        length = np.sum(data[:,-1])
+
+        phases = np.arctan2(data[:,0], data[:,1])/np.pi * 360
+        shape = np.vstack([phases, amplitudes]).T
+
+        return max_amp, length, shape
 
 
     def start(self, MP: bool=True, CPUs: int=None, traject: bool=False) \
